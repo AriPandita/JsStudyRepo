3d55b333d989559530a3f4f90ff966f7c6a170b1
AriPandita <aripandita2@gmail.com>
2023-09-04 21:29:20 +0800

    membuat folder perulangan

---
+++ Perulangan/index.js
@@ -0,0 +1,19 @@
+
+//================== LOOPING ==================//
+
+//for
+
+for (let a = 1; a <= 10; a++) {
+    document.write(`FOR ke ${a}, `);
+}
+
+document.write('<br>');
+
+//while
+let b = 1;
+while (b <= 10){
+    document.write(`While ke ${b}, `);
+    b++;
+}
+
+//================== LOOPING END ===============//
--- index.js
+++
@@ -1,5 +0,0 @@
-// Looping
-
-for (let i = 1; i <= 10; i++) {
-    document.write(`<p>Perulangan ke ${i}</p>`);
-}
