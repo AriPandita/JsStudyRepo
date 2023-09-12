e623dbc124b202ae7e0fe64dfdb18e4d592063e3
AriPandita <aripandita2@gmail.com>
2023-09-12 21:12:20 +0800

    Function with parameter

---
+++ basic/function-parameter.js
@@ -0,0 +1,13 @@
+
+// Function with parameter 
+function fullName(firtsName, lastName){
+    document.writeln(`Hello ${firtsName} ${lastName}`);
+}
+
+fullName('John', 'Doe');
+
+document.writeln("<br>");
+
+namaAwal = "Ari";
+namaAkhir = "Pandita";
+fullName(namaAwal, namaAkhir);
